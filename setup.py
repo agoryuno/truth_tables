552293581224286fb27ec9603ec7122f531e0e16
Alexander Goryunov <43528668+agoryuno@users.noreply.github.com>
2023-01-11 16:32:50 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='truth_table',
+    version='0.0.0.1',
+    packages=['truth_table'],
+    url='',
+    license='',
+    author='',
+    description='',
+    requires_python='>=3.8'
+    )
