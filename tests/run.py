63eaa4a96b899607ec63b8cc646822274d411386
Alexander Goryunov <alex.goryunov@gmail.com>
2023-01-12 10:19:34 +0200

    errors

--- tests/run.py
+++ tests/run.py
@@ -3,34 +3,32 @@ from truth_table import _and, _or, _not, _xor, _symd, _diff, _if, Value, build_t
 
 
 def set_var(var, val):
-	if val:
-		var.true
-		return
-	var.false
+    if val:
+        var.true
+        return
+    var.false
 
 
 class MyTests(unittest.TestCase):
     def setUp(self):
-        a = Value("A")
-        b = Value("B")
-        c = Value("C")
+        self.a = Value("A")
+        self.b = Value("B")
+        self.c = Value("C")
 
     def test_if(self):
-        stmt1 = _if(a, b)
-        stmt2 = _or(_not(a), b)
+        stmt1 = _if(self.a, self.b)
+        stmt2 = _or(_not(self.a), self.b)
 
         vals = [[True, True], [True, False], [False, True],
-        		[False, False]]
+                [False, False]]
         for av, bv in vals:
-        	set_var(a, av)
-        	set_var(b, bv)
-        	self.assertEqual(stmt1 == stmt2)
+            set_var(self.a, av)
+            set_var(self.b, bv)
+            self.assertEqual(stmt1 == stmt2)
 
     def test_method2(self):
-        # test method 2 code and assertions
+        ...
 
-    def tearDown(self):
-        # code to be executed after each test method
 
 if __name__ == '__main__':
     unittest.main()
