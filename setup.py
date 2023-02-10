59fff9eab10cbdb45f13538c199e0cd9310ac505
Alexander Goryunov <43528668+agoryuno@users.noreply.github.com>
2023-02-10 11:59:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='truth_table',
     version='0.0.0.1',
     packages=['truth_table'],
-    url='',
+    url='https://github.com/agoryuno/truth_tables',
     license='',
-    author='',
+    author='Alexander Goryunov',
     description='',
     requires_python='>=3.8'
     )
